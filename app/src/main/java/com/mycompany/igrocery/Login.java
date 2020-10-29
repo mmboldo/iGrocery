4ba8251cefdd0037b52b0b86a6d8c57292d687d3
julianatakase2904 <takasej@student.douglascollege.ca>
2020-10-29 11:27:25 -0700

    Firebase login and register

--- app/src/main/java/com/mycompany/igrocery/Login.java
+++ app/src/main/java/com/mycompany/igrocery/Login.java
@@ -1,26 +1,50 @@
 package com.mycompany.igrocery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    Button loginBtn, newUserBtn, goToStoreMap;
+    private AppCompatButton loginBtn, newUserBtn, goToStoreMap;
+    private FirebaseAuth auth;
+    private TextInputEditText emailInput, passInput;
+    private TextInputLayout emailLayout, passLayout;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //get instance of Firebase
+        auth = FirebaseAuth.getInstance();
+
         //find the ids
-        loginBtn = (Button)findViewById(R.id.loginBtn);
-        newUserBtn = (Button) findViewById(R.id.newUserBtn);
-        goToStoreMap = (Button) findViewById(R.id.tempBtn);
+        emailLayout = (TextInputLayout) findViewById(R.id.emailInputLayout);
+        passLayout = (TextInputLayout) findViewById(R.id.passwordInputLayout);
+        emailInput = (TextInputEditText) findViewById(R.id.email);
+        passInput = (TextInputEditText) findViewById(R.id.password);
+        loginBtn = (AppCompatButton)findViewById(R.id.loginBtn);
+        newUserBtn = (AppCompatButton) findViewById(R.id.newUserBtn);
+        goToStoreMap = (AppCompatButton) findViewById(R.id.tempBtn);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         //Go to new User Registration
         newUserBtn.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +54,7 @@ public class Login extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
         //Go to Store Map
         goToStoreMap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,5 +64,88 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        //login btn call method
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginForm();
+            }
+        });
+
+    }
+
+    private void loginForm() {
+        String email = emailInput.getText().toString().trim();
+        String pass = passInput.getText().toString().trim();
+
+        if(!checkEmail()){
+            return;
+        }
+        if(!checkPassword()){
+            return;
+        }
+        emailLayout.setErrorEnabled(false);
+        passLayout.setErrorEnabled(false);
+        progressBar.setVisibility(View.VISIBLE);
+
+        auth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+
+                        if(!task.isSuccessful()){
+                            Toast.makeText(Login.this,
+                                    getString(R.string.loginError), Toast.LENGTH_SHORT).show();
+                        } else{
+                            startActivity(new Intent(Login.this, CreateList.class));
+                            finish();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private boolean checkEmail() {
+        String email = emailInput.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+            emailLayout.setErrorEnabled(true);
+            emailLayout.setError(getString(R.string.emailError));
+            emailInput.setError(getString(R.string.requiredMsg));
+            requestFocus(emailInput);
+            return false;
+        }
+        emailLayout.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+        String password = passInput.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+            passLayout.setError(getString(R.string.passError));
+            passInput.setError(getString(R.string.requiredMsg));
+            requestFocus(passInput);
+            return false;
+        }
+        passLayout.setErrorEnabled(false);
+        return true;
+    }
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+    private static boolean isPasswordValid(String password){
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
     }
 }
--- app/src/main/java/com/mycompany/igrocery/Register.java
+++ app/src/main/java/com/mycompany/igrocery/Register.java
@@ -1,22 +1,62 @@
 package com.mycompany.igrocery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
-    Button alreadyUserBtn;
+
+    private FirebaseAuth auth;
+    private TextInputEditText registerEmail;
+    private TextInputEditText registerPass;
+    private TextInputEditText registerPassConfirm;
+    private AppCompatButton registerBtn;
+    private AppCompatButton alreadyUserBtn;
+    private AppCompatButton addAddressBtn;
+    private ProgressBar progressBar;
+    private TextInputLayout registerInputLayoutEmail;
+    private TextInputLayout registerInputLayoutPass;
+    private TextInputLayout registerInputLayoutPassConfirm;
+
+    private static final String TAG = "FIREBASE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //get the firebase obj
+        auth = FirebaseAuth.getInstance();
+
         //find the id
-        alreadyUserBtn = (Button) findViewById(R.id.alreadyUserBtn);
+        registerEmail = (TextInputEditText) findViewById(R.id.registrationEmail);
+        registerPass = (TextInputEditText) findViewById(R.id.registrationPassword);
+        registerPassConfirm = (TextInputEditText) findViewById(R.id.registrationPassConfirm);
+        registerBtn = (AppCompatButton) findViewById(R.id.registerBtn);
+        alreadyUserBtn = (AppCompatButton) findViewById(R.id.alreadyUserBtn);
+        addAddressBtn = (AppCompatButton) findViewById(R.id.addAddress);
+        registerInputLayoutEmail = (TextInputLayout) findViewById(R.id.registerInputLayoutEmail);
+        registerInputLayoutPass = (TextInputLayout) findViewById(R.id.registerInputLayoutPass);
+        registerInputLayoutPassConfirm = (TextInputLayout) findViewById(R.id.registerInputLayoutPassConfirm);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         //Go to Login
         alreadyUserBtn.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +66,114 @@ public class Register extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+        //send registration
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitForm();
+            }
+        });
     }
+
+    private void submitForm() {
+        String email = registerEmail.getText().toString().trim();
+        String password = registerPass.getText().toString().trim();
+
+        if(!checkEmail()) {
+            return;
+        }
+        if(!checkPassword()) {
+            return;
+        }
+        if(!checkPasswordConfirm()) {
+            return;
+        }
+
+        registerInputLayoutEmail.setErrorEnabled(false);
+        registerInputLayoutPass.setErrorEnabled(false);
+        registerInputLayoutPassConfirm.setErrorEnabled(false);
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        //create a user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "user creation: Complete");
+
+                        progressBar.setVisibility(View.GONE);
+
+                        if(!task.isSuccessful()) {
+                            Log.d(TAG, "Creation failure." + task.getException());
+                        } else {
+                            startActivity(new Intent(Register.this, Login.class));
+                            finish();
+                        }
+                    }
+                });
+        Toast.makeText(getApplicationContext(), "Thank you for register!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private boolean checkEmail() {
+        String email = registerEmail.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+            registerInputLayoutEmail.setErrorEnabled(true);
+            registerInputLayoutEmail.setError(getString(R.string.emailError));
+            registerEmail.setError(getString(R.string.requiredMsg));
+            requestFocus(registerEmail);
+            return false;
+        }
+        registerInputLayoutEmail.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+        String password = registerPass.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+            registerInputLayoutPass.setError(getString(R.string.passError));
+            registerPass.setError(getString(R.string.requiredMsg));
+            requestFocus(registerPass);
+            return false;
+        }
+        registerInputLayoutPass.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPasswordConfirm() {
+        String passwordConfirm = registerPassConfirm.getText().toString().trim();
+        String password = registerPass.getText().toString().trim();
+        if (!passwordConfirm.equals(password)) {
+            registerInputLayoutPassConfirm.setError(getString(R.string.passErrorConfir));
+            registerPassConfirm.setError(getString(R.string.requiredMsg));
+            requestFocus(registerPassConfirm);
+            return false;
+        }
+        registerInputLayoutPassConfirm.setErrorEnabled(false);
+        return true;
+    }
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private static boolean isPasswordValid(String password){
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+
+
 }
