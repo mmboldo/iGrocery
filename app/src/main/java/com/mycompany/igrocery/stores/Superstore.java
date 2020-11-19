14cb3cba3e85df220c11e30bd9b30cac7f1585b0
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-18 17:46:56 -0800

    Created Superstore class, containing information on Real Canadia Superstore

---
+++ app/src/main/java/com/mycompany/igrocery/stores/Superstore.java
@@ -0,0 +1,25 @@
+package com.mycompany.igrocery.stores;
+
+public class Superstore {
+
+    public String name = "Real Canadian Superstore";
+    public String address = "1105- 4700 Kingsway";
+    public float longitude = (float) 49.227826;
+    public float latitude = (float) -123.001253;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+}
