afec235651361a88684ff2ddaf178598aaf059ec
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-18 17:52:01 -0800

    Added SaveOnFoods.java class

---
+++ app/src/main/java/com/mycompany/igrocery/stores/SaveOnFoods.java
@@ -0,0 +1,25 @@
+package com.mycompany.igrocery.stores;
+
+public class SaveOnFoods {
+
+    public String name = "Save On Foods";
+    public String address = "7015 120 St";
+    public float latitude = (float) 49.130766;
+    public float longitude = (float) -122.892993;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+}
