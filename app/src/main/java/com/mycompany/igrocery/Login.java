474ede4525525268fd22b1325adcb4cba52f0e4d
julianatakase2904 <takasej@student.douglascollege.ca>
2020-11-23 21:46:40 -0800

    Update with Marcelo's branch

--- app/src/main/java/com/mycompany/igrocery/Login.java
+++ app/src/main/java/com/mycompany/igrocery/Login.java
@@ -21,7 +21,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    private AppCompatButton loginBtn, newUserBtn, goToStoreMap, goToCalendar;
+    private AppCompatButton loginBtn, newUserBtn, goToCalendar;
     private FirebaseAuth auth;
     private TextInputEditText emailInput, passInput;
     private TextInputLayout emailLayout, passLayout;
@@ -42,9 +42,8 @@ public class Login extends AppCompatActivity {
         passInput = (TextInputEditText) findViewById(R.id.password);
         loginBtn = (AppCompatButton)findViewById(R.id.saveBtn);
         newUserBtn = (AppCompatButton) findViewById(R.id.newUserBtn);
-        goToStoreMap = (AppCompatButton) findViewById(R.id.tempBtn);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        goToStoreMap = (AppCompatButton)findViewById(R.id.tempBtn2);
+        goToCalendar = (AppCompatButton)findViewById(R.id.tempBtn2);
 
         //temp button only for test
 
@@ -58,17 +57,9 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        //Go to Store Map
-        goToStoreMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(Login.this, StoreMap.class);
-                startActivity(myIntent);
-            }
-        });
 
         //Go to Calendar
-        goToStoreMap.setOnClickListener(new View.OnClickListener() {
+        goToCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(Login.this, Calendar.class);
