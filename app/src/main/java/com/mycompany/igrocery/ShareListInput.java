07dd4daa344368bfeeac56d84b539741330b4dbd
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-23 22:22:26 -0800

    Added new files to create a share list

---
+++ app/src/main/java/com/mycompany/igrocery/ShareListInput.java
@@ -0,0 +1,14 @@
+package com.mycompany.igrocery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareListInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_list_input);
+    }
+}
