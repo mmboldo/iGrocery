6ba6fa59f00c61b21367e83dbcc7dece868e3399
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-25 18:10:54 -0800

    Added a few changes to SharedList.java

--- app/src/main/java/com/mycompany/igrocery/SharedList.java
+++ app/src/main/java/com/mycompany/igrocery/SharedList.java
@@ -2,12 +2,22 @@ package com.mycompany.igrocery;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,15 +28,30 @@ import java.util.ArrayList;
 
 public class SharedList extends AppCompatActivity {
 
+    //Initialize Drawer Navigation variable
+    DrawerLayout drawerLayout;
+
+    //Initialize variables
     DatabaseReference reference;
     RecyclerView sharedList; //ourdoes
     ArrayList<SharedListUser> sharedListUser; //list
     SharedListAdapter sharedListAdapter;
 
+    private FirebaseUser user;
+    private String userEmail;
+
+    public void getCurrentUser(){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userEmail = user.getEmail().replace(".", "&");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shared_list);
+
+        //getting information on user who is logged in
+        getCurrentUser();
 
         //working with data
         sharedList = findViewById(R.id.rvSharedList);
@@ -34,7 +59,7 @@ public class SharedList extends AppCompatActivity {
         sharedListUser = new ArrayList<SharedListUser>();
 
         //get data from firebase
-        reference = FirebaseDatabase.getInstance().getReference().child("ListPermissions");
+        reference = FirebaseDatabase.getInstance().getReference().child("ListPermissions").child("userEmail: " + userEmail);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -55,5 +80,98 @@ public class SharedList extends AppCompatActivity {
             }
         });
 
+        //Drawer Navigation
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar_view);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
     }
+
+
+    // START: Methods for Nav menu
+    public void ClickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickList(View view){
+        recreate();
+    }
+    public void ClickSearch(View view){
+        redirectActivity(this, PermissionActivity.class);
+    }
+    public void ClickLogout(View view){
+        logout(this);
+    }
+    public void ClickCalendar(View view) {
+        redirectActivity(this, Calendar.class);
+    }
+    public void ClickSharedList(View view) {
+        redirectActivity(this, SharedList.class);
+    }
+
+    // This adds the navigation functionality for the main_nav_drawer Share my list menu link
+    public void shareMyList(View view) {
+        Intent intent2 = new Intent(SharedList.this, ShareList.class);
+        startActivity(intent2);
+    }
+
+    public void ClickStoreMap(View view) {
+        redirectActivity(this, StoreMap.class);
+    }
+
+    public void logout(Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //quit the iGrocery app
+                /*activity.finishAffinity();
+                System.exit(0);*/
+
+                //Back to login page
+                Intent intent = new Intent(SharedList.this, Login.class);
+                activity.startActivity(intent);
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class aclass) {
+        Intent intent = new Intent(activity, aclass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //close drawer
+        closeDrawer(drawerLayout);
+    }
+    // FINISH: Methods for Nav menu
 }
