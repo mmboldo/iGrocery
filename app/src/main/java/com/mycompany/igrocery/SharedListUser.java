24574d744ef357f9dcd0b9256dcca3477e34ddda
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-25 17:06:37 -0800

    Created SharedListUser.java, a getter/setter/constructor class

---
+++ app/src/main/java/com/mycompany/igrocery/SharedListUser.java
@@ -0,0 +1,21 @@
+package com.mycompany.igrocery;
+
+public class SharedListUser {
+
+    String email;
+
+    public SharedListUser() {
+    }
+
+    public SharedListUser(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
