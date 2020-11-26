7c632c847423231a2c7ff87b01fcc4184b8d4304
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-25 16:59:19 -0800

    Created Share List class and xml

---
+++ app/src/main/java/com/mycompany/igrocery/SharedList.java
@@ -0,0 +1,14 @@
+package com.mycompany.igrocery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SharedList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_list);
+    }
+}
